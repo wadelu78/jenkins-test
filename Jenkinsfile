b89654b0a9227cd8d72554b0b5ad44e0a1f8a2ce
wadelu78 <wadelu0714@gmail.com>
2026-07-20 15:19:51 +1200

    Update Jenknsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -47,7 +47,7 @@ pipeline {
 
     post {
       always {
-        junit 'jest-results/junit.xml'
+        junit 'test-results/e2e-junit.xml'
       }
     }
 }
--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['junit']
+    ['junit', { outputFile: 'test-results/e2e-junit.xml' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
